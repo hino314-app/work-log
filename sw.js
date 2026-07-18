7af554f82208f268040694ae80efc6eb7802e626
hino314-app <yopidamina@yahoo.co.jp>
2026-07-18 20:53:40 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,26 +1,10 @@
-const CACHE="time-block-v2-20260718b";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
-
-self.addEventListener("install",event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
+self.addEventListener("install",()=>self.skipWaiting());
 self.addEventListener("activate",event=>{
-  event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch",event=>{
-  event.respondWith(
-    fetch(event.request)
-      .then(response=>{
-        const copy=response.clone();
-        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-        return response;
-      })
-      .catch(()=>caches.match(event.request).then(r=>r||caches.match("./index.html")))
-  );
+  event.waitUntil((async()=>{
+    const keys=await caches.keys();
+    await Promise.all(keys.map(key=>caches.delete(key)));
+    await self.registration.unregister();
+    const pages=await self.clients.matchAll({type:"window"});
+    pages.forEach(page=>page.navigate(page.url));
+  })());
 });
