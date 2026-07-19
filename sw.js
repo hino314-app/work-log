5f98468df102d27b27cf50f80f8a45ecb007821e
hino314-app <yopidamina@yahoo.co.jp>
2026-07-19 11:58:38 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,5 @@
-self.addEventListener("install",()=>self.skipWaiting());
-self.addEventListener("activate",event=>{
-  event.waitUntil((async()=>{
-    const keys=await caches.keys();
-    await Promise.all(keys.map(key=>caches.delete(key)));
-    await self.registration.unregister();
-    const pages=await self.clients.matchAll({type:"window"});
-    pages.forEach(page=>page.navigate(page.url));
-  })());
-});
+const CACHE="time-block-v12";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))))});
