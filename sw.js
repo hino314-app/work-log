c84b2224dd286ac91ff716a0c87480de7b6b3fcf
hino314-app <yopidamina@yahoo.co.jp>
2026-07-20 13:08:48 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="time-block-v17";
+const CACHE="time-block-v18";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",event=>{
